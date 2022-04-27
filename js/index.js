a5814593a78f24361280b56fd9cd5af830931739
Milos Djordjevic <milos.shomy95@hotmail.com>
2022-04-27 15:41:25 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const adviceNumber = document.querySelector(".advice-number");
+const adviceText = document.querySelector(".advice-text");
+const btn = document.querySelector(".submit-btn");
+const box = document.querySelector(".box");
+
+const addAdvice = () => {
+  fetch("https://api.adviceslip.com/advice").then((res) => {
+    res.json().then((data) => {
+      console.log(data);
+      let slip = data.slip;
+      let advice = slip.advice;
+      let adviceId = slip.id;
+      adviceNumber.textContent = `Advice #${adviceId}`;
+      adviceText.textContent = `"${advice}"`;
+    });
+  });
+};
+
+btn.addEventListener("click", addAdvice);
+
+window.addEventListener("load", addAdvice);
